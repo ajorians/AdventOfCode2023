e0a0ab39460b42ef781521ee172c43a77090f760
A.J. Orians <ajorians@gmail.com>
2023-12-07 22:39:58 -0500

    Day 6.

---
+++ Day6/src/main.rs
@@ -0,0 +1,32 @@
+fn race(time : i32, record : i32) ->i32 {
+
+    let mut times_broke_record = 0;
+
+    for buildup in 0..time
+    {
+        let remainingTime = time - buildup;
+        let speed = buildup;
+
+        let distance_traveled = remainingTime * speed;
+
+        if distance_traveled > record
+        {
+            times_broke_record+=1;
+        }
+    }
+
+    return times_broke_record;
+}
+
+fn main() {
+    println!("Day6");
+
+    let times_broke_record1 = race(  38 /*time*/, 234 /*record*/ );
+    let times_broke_record2 = race(  67 /*time*/, 1027 /*record*/ );
+    let times_broke_record3 = race(  76 /*time*/, 1157 /*record*/ );
+    let times_broke_record4 = race(  73 /*time*/,1236 /*record*/ );
+
+    println!("Mutiple of values is {}", times_broke_record1*times_broke_record2*times_broke_record3*times_broke_record4);
+}
+
+
